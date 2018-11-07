688f9ce1cd8602800a4b6ba426ceb03f0f07c603
Kofi Amoussou <karsoft92@gmail.com>
2018-11-07 06:56:48 +0000

    added blog, added urls, included bootstrap 4 and scripts, added if logic

--- src/app.js
+++ src/app.js
@@ -3,19 +3,46 @@
 // requiring express
 var express = require('express'),
 posts = require('./mock/posts.json');
+var pug = require('pug');
+
+	//turning an object into array
+var postsLists = Object.keys(posts).map(function(value){
+ return posts[value]});
+
 
 //creating the app
-var app = express();
+var app = express(); 
 
+// app.use() defines middleware
+app.use('/static',express.static(__dirname + '/public/'));
+app.set('view engine','pug');
+app.set('views', __dirname + '/templates');
+
+
+
+debugger;
 
 //creating route
 
 app.get("/",function(req,res){
-		res.send('<h1>I love myself</h1>');
+		var path = req.path;
+		res.locals.path = path;
+		res.render('index');
 });
 
-app.get("/blog",function(req,res){
-	 res.send(posts);
+// app.get("/blog",function(req,res){
+// 	 res.send(posts);
+// });
+
+app.get("/blog/:title?",function(req,res){
+	var title = req.params.title; // grab post based on title
+	if(title === undefined){
+		res.status(503); //sending server status code
+		res.render('blog', {posts: postsLists})
+	}else{
+	var post = posts[title] || {};
+	res.render('post',{post: post});
+		}		
 });
 
 //listening to app
